0227bc1abad87d161362c2c019eb16324d9ecb3f
Alvin <alvin@agideo.com>
2015-08-27 14:40:00 +0800

    add dialog

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ var TodoApp = React.createClass({
       levelz: ["成人", "儿童"],
       levely: ["25座", "35座"],
       levelx: ["如家三星", "汉庭四星", "喜来登五星"],
-      showDialog: false
+      data: [
+        {z: "成人", y: "25座", x: "如家三星", price: 120},
+        {z: "成人", y: "35座", x: "如家三星", price: 380},
+        {z: "儿童", y: "25座", x: "喜来登五星", price: 780},
+        {z: "儿童", y: "35座", x: "如家三星", price: 180},
+        {z: "成人", y: "25座", x: "汉庭四星", price: 280}
+      ],
+      showDialog: true
     }
   },
   handleClick: function(e) {
@@ -29,8 +36,47 @@ var TodoApp = React.createClass({
 
     var renderDialog = function() {
       return (
-        <Dialog visible={_this.state.showDialog} onClose={_this.handleDailogClose} >
-          <p>first dialog</p>
+        <Dialog style={{wdith: 300}} title="选择价格" visible={_this.state.showDialog} onClose={_this.handleDailogClose} >
+          <fieldset>
+            <legend>用餐</legend>
+            <label><input type="checkbox" />成人 25</label>
+            <label><input type="checkbox" />儿童 15</label>
+          </fieldset>
+          <fieldset>
+            <legend>住</legend>
+            <label><input type="checkbox" />如家三星 183</label>
+            <label><input type="checkbox" />汉庭四星 383</label>
+            <label><input type="checkbox" />喜来登五星 598</label>
+          </fieldset>
+          <fieldset>
+            <legend>大交通</legend>
+            <label><input type="checkbox" />飞机 经济舱 383</label>
+            <label><input type="checkbox" />飞机 商务舱 583</label>
+            <label><input type="checkbox" />动车 二等 298</label>
+            <label><input type="checkbox" />动车 一等 398</label>
+          </fieldset>
+          <fieldset>
+            <legend>小交通</legend>
+            <label><input type="checkbox" />大巴57座 </label>
+            <label><input type="checkbox" />小巴25座</label>
+          </fieldset>
+          <fieldset>
+            <legend>门票</legend>
+            <label><input type="checkbox" />大沙岙 80 </label>
+            <label><input type="checkbox" />太姥山成人 100</label>
+            <label><input type="checkbox" />儿童 80</label>
+            <label><input type="checkbox" />老人 5</label>
+          </fieldset>
+          <fieldset>
+            <legend>其他</legend>
+            <label><input type="checkbox" />包 4</label>
+            <label><input type="checkbox" />帽子 5</label>
+            <label><input type="checkbox" />箱子 15</label>
+            <label><input type="checkbox" />导游服务 30</label>
+            <label><input type="checkbox" />保险 5</label>
+          </fieldset>
+
+          <button onClick={_this.handleDailogClose}>确定</button>
         </Dialog>
       );
     }
